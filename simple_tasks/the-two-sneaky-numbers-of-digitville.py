156d34c3edd3c3a84d78d5118edef647ccce5a53
Artists <arists228.grogy@gmail.com>
2025-11-01 00:58:04 +0300

    1 task
    1 easy task

---
+++ simple_tasks/the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getSneakyNumbers(self, nums):
+        self.num = []
+        for item in nums: 
+            if item not in self.num:
+                self.num.append(item)                   
+        return self.num
+    
+
+
+nums = [0, 0, 1, 1]
+solution = Solution()
+print(solution.getSneakyNumbers(nums))
+            
+                
